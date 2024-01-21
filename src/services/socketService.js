cb14d4e3e638b1269122e79f8ff86558f6133295
PEYZ <zhiani2000@gmail.com>
2024-01-21 02:12:44 -0800

    added socket basics

---
+++ src/services/socketService.js
@@ -0,0 +1,34 @@
+const { Server } = require("socket.io");
+
+let io;
+
+function init(httpServer) {
+    io = new Server(httpServer, {
+        cors: {
+            origin: "*", // Set this to your frontend URL in production
+            methods: ["GET", "POST"]
+        }
+    });
+
+    io.on('connection', (socket) => {
+        console.log('New client connected', socket.id);
+
+        socket.on('disconnect', () => {
+            console.log('Client disconnected');
+        });
+
+        // Additional event handlers can be added here
+    });
+}
+
+function getIO() {
+    if (!io) {
+        throw new Error("Socket.io not initialized!");
+    }
+    return io;
+}
+
+module.exports = {
+    init,
+    getIO
+};
