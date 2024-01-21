c5ea3251ffcd5868c52a99f3d1c69849574e3971
PEYZ <zhiani2000@gmail.com>
2024-01-21 09:50:48 -0800

    added mongo get all endpoint for the purpose of distirbtuion of noise data

--- src/server.js
+++ src/server.js
@@ -1,49 +1,57 @@
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
-const connectDB = require('./services/mongodb');
-const socketService = require('./services/socketService');
-const User = require('../models/UserModel');
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const connectDB = require("./services/mongodb");
+const socketService = require("./services/socketService");
+const User = require("../models/UserModel");
 
 const app = express();
 const server = http.createServer(app);
 
-require('dotenv').config();
+require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('Noise Map Backend is running!');
+app.get("/", (req, res) => {
+  res.send("Noise Map Backend is running!");
 });
 
-app.post('/collect-noise-data', async (req, res) => {
+app.post("/collect-noise-data", async (req, res) => {
   try {
-    const noiseData = req.body['noise'];
-    const longitude = req.body['long'];
-    const latitude = req.body['lat'];
+    const noiseData = req.body["noise"];
+    const longitude = req.body["long"];
+    const latitude = req.body["lat"];
 
     let timestamp;
-    if(req.body.hasOwnProperty('timestamp')){
-      timestamp = new Date(req.body['timestamp']);
+    if (req.body.hasOwnProperty("timestamp")) {
+      timestamp = new Date(req.body["timestamp"]);
     } else {
       timestamp = Date.now();
     }
-    
-    const userStore = await User.create({ long:longitude, lat:latitude, noise: noiseData, timestamp: timestamp });
-    console.log("received:",{ long:longitude, lat:latitude, noise: noiseData, timestamp: timestamp })
+
+    const userStore = await User.create({
+      long: longitude,
+      lat: latitude,
+      noise: noiseData,
+      timestamp: timestamp,
+    });
+    console.log("received:", {
+      long: longitude,
+      lat: latitude,
+      noise: noiseData,
+      timestamp: timestamp,
+    });
 
     // Broadcast noise data to connected clients
     const io = socketService.getIO();
-    io.emit('newNoiseData', { longitude, latitude, noiseData, timestamp });
+    io.emit("newNoiseData", { longitude, latitude, noiseData, timestamp });
 
-    
-    res.status(200).send('Data received');
-
+    res.status(200).send("Data received");
   } catch (error) {
     console.log(error);
-    res.status(500).send('Error processing data');
+    res.status(500).send("Error processing data");
   }
 });
 
@@ -52,10 +60,10 @@ app.get("/get-sounds", async (req, res) => {
     const users = await User.find({});
     res.status(200).json(users);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: "Error fetching data" });
   }
 });
-
 const port = process.env.PORT || 8080;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
