8832cbd85927713311cb5ae72dc7aaf7c5fca5fc
PEYZ <zhiani2000@gmail.com>
2024-01-21 01:33:05 -0800

    initialization for socket.io

--- src/server.js
+++ src/server.js
@@ -1,56 +1,50 @@
 const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const connectDB = require('./services/mongodb');
+const socketService = require('./services/socketService');
+const User = require('../models/UserModel');
+
 const app = express();
-const port = process.env.PORT || 8080;
-const connectDB = require('./services/mongodb'); 
-const User = require('../models/UserModel'); 
-
-
+const server = http.createServer(app);
 
 require('dotenv').config();
-
 app.use(express.json());
-
-const cors = require('cors');
 app.use(cors());
 
-
 connectDB();
-
 
 app.get('/', (req, res) => {
   res.send('Noise Map Backend is running!');
 });
 
-// Example POST route for receiving data
 app.post('/collect-noise-data', async (req, res) => {
   try {
-    const noiseData = req.body['noise'];
-    const longitude = req.body['long'];
-    const latitude = req.body['lat'];
-    console.log(noiseData);
-    const userStore = await User.create({ long:longitude, lat:latitude, noise: noiseData });
+    const { noise, long, lat } = req.body;
+    console.log(noise);
+    const userStore = await User.create({ long, lat, noise });
     res.status(200).send('Data received');
+
+    // Broadcast noise data to connected clients
+    const io = socketService.getIO();
+    io.emit('newNoiseData', { long, lat, noise });
 
   } catch (error) {
     res.status(500).send('Error processing data');
   }
 });
 
-//GET user coordinates
-app.get("/get-sounds", async(req,res) => {
-  try{
-    const users = await User.find({})
-    res.status(200).json(users)
-
+app.get("/get-sounds", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-  catch(error){
-    res.status(500).json({message: error.message})
-
-  }
-})
-
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
 
+const port = process.env.PORT || 8080;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  socketService.init(server);
+});
